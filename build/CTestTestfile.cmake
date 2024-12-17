6188e4782fd900d118bfa17ef374de8c929df80b
Bogdan-Cpp <bogdan1000leitan2007@gmail.com>
2024-12-17 09:09:57 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Bogdan/Documents/Console Forge
-# Build directory: C:/Users/Bogdan/Documents/Console Forge/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
